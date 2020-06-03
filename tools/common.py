1ee4c18eb02e50256748ba914e3b9ac196d600e0
YangLiu14 <lylander14@gmail.com>
2020-06-03 18:02:02 +0800

    add utils function to list all files in a folder

---
+++ tools/common.py
@@ -0,0 +1,22 @@
+"""common.py: common tools"""
+__author__ = "Yang Liu"
+__email__ = "lander14@outlook.com"
+
+import glob
+import os
+import tqdm
+
+
+# Template function
+def list_files_in_dir(root_dir: str, file_type=".jpg"):
+    # List all the folders under the root_dir
+    folders = [fn.split('/')[-1] for fn in sorted(glob.glob(os.path.join(root_dir, '*')))]
+
+    for idx, folder in enumerate(tqdm.tqdm(folders)):
+        fpath = os.path.join(root_dir, folder)
+        # List all files in the current folder
+        files = sorted(glob.glob(fpath + '/*' + file_type))
+        # =================================
+        # Do the rest stuffs as you please
+        # =================================
+
