56c789dd90bac66ebaa17205146dfd128d7198b4
Yang Liu <yang.liu@cirrantic.com>
2020-10-15 15:51:15 +0200

    add func to combine images into image tile

--- tools/image_utils.py
+++ tools/image_utils.py
@@ -3,10 +3,46 @@ __author__ = "Yang Liu"
 __email__ = "lander14@outlook.com"
 
 import colorsys
+import cv2
 import numpy as np
 import os
 import png as pypng  # pip install pypng
 import PIL.Image as Image
+
+
+def image_stitching(image_paths, rows, cols, out_path):
+    """
+    Stitch list of images into (rows x cols) image-tiles.
+    Args:
+        image_paths: List of image paths, ordered in the fashion that, the 1st image with be placed at (0,0)
+                     in the resulting image tile, the 2nd image at(0,1), 3rd at (0,2) and so on.
+        rows: Int, number of rows in the resulting image tile.
+        cols: Int, number of columns in the resulting image tile.
+        out_path: Str, output path and file name.
+    """
+    assert rows * cols == len(image_paths)
+    # Read images as numpy arrays
+    img_list = list()
+    # for path in image_paths:
+    #     img = Image.open(path)
+    #     img_np = np.array(img.getdata())
+    #     img_list.append(img_np)
+    img_list = [cv2.imread(filename) for filename in image_paths]
+
+    # combine images vertically
+    img_vert = list()
+
+    while img_list:
+        curr_row = img_list[:3]
+        img_list = img_list[3:]
+        combined_img = np.hstack(curr_row)
+        img_vert.append(combined_img)
+    # combine images horizontally
+    all_combined = np.vstack(img_vert)
+
+    # Save image
+    cv2.imwrite(out_path, all_combined)
+
 
 
 # ================================================
@@ -62,3 +98,15 @@ def store_masks_to_png(masks, outdir: str):
         writer = pypng.Writer(width=img_size[1], height=img_size[0], bitdepth=16)
         png2list = png.tolist()
         writer.write(f, png2list)
+
+
+if __name__ == "__main__":
+    image_paths = ["COCOunknown_score.png", "COCOunknown_bg.png", "COCOunknown_(1-bg).png",
+                   "COCOneighbor_score.png", "COCOneighbor_bg.png", "COCOneighbor_(1-bg).png",
+                   "COCOknown_score.png", "COCOknown_bg.png", "COCOknown_(1-bg).png"]
+    root_dir = ""
+    for i in range(len(image_paths)):
+        image_paths[i] = root_dir + image_paths[i]
+
+    output_path = "combined.png"
+    image_stitching(image_paths, 3, 3, output_path)
